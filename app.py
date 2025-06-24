4763cc90c621b7e045dc48914eaaca8c06f515f3
KimDaniel22 <kimdaniel2204@gmail.com>
2025-06-24 13:51:09 +0300

    switch asyncio

--- app.py
+++ app.py
@@ -5,18 +5,28 @@ from telegram import Update
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return 'Bot is alive!'
+
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     update = request.get_json()
     telegram_update = Update.de_json(update, application.bot)
-    asyncio.run(application.process_update(telegram_update))
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(application.process_update(telegram_update))
+    loop.close()
+
     return '', 200
 
 
-@app.before_request
+@app.before_first_request
 def init_webhook():
-    asyncio.run(set_webhook())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(set_webhook())
+    loop.close()
